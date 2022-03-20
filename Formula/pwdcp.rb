69cd1cea377f0cd510092caadd3bd679081d5011
ZhangXiao <nealse.zh@gmail.com>
2022-03-20 22:23:14 +0800

    add a version

--- Formula/pwdcp.rb
+++ Formula/pwdcp.rb
@@ -2,6 +2,8 @@ class Pwdcp < Formula
   desc "Simple script coping current path to clipboard" 
   homepage "nearxdu.github.io"
   url "https://github.com/NearXdu/homebrew-pwdcp", :using => :git
+  version '0.0.1'
+
 
   def install
 
